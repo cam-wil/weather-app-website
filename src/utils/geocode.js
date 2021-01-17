1cebd51c5b7555d2a164a02c49e33a8683d374b2
Cameron Wilson <61340066+cam-wil@users.noreply.github.com>
2021-01-16 21:21:20 -0600

    Update geocode.js
    added token var, changed and removed leaked token

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
-
+const api_token = ''
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiY2FtZXIiLCJhIjoiY2tpY29neThsMGtrYjJ3cDNudnp3Y2w4YyJ9.wrvp_lA7Oq7XuNOHwHUxNQ'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + api_token
     
     request({ url: url, json: true}, (error, {body}) => {
         if (error) {
